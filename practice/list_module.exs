25e2aec297c19e2dea20b2dd255a66773a4e590e
fargelus <ddraudred@gmail.com>
2022-01-31 07:03:43 +0300

    Range example added

--- practice/list_module.exs
+++ practice/list_module.exs
@@ -4,17 +4,58 @@ defmodule ListModule do
   def len([_ | tail]) do
     1 + length(tail)
   end
-end
 
-defmodule ListModuleTest do
-  def test_len do
-    [] |> ListModule.len |> IO.puts # 0
-    [1000] |> ListModule.len |> IO.puts # 1
+  def range(from, to)
+    when not is_integer(from) or not is_integer(to) do
+    {:error, "parameters must be integer"}
+  end
 
-    [1, 2, 3, -123, 'afsd', 555]
-      |> ListModule.len
-      |> IO.puts # 6
+  def range(from, from), do: [from]
+
+  def range(from, to) do
+    if from > to do
+      [from] ++ range(from - 1, to)
+    else
+      [from] ++ range(from + 1, to)
+    end
   end
 end
 
-ListModuleTest.test_len
+defmodule ListModuleTest do
+  def test do
+    test_len()
+    IO.puts ""
+    test_range()
+  end
+
+  def test_len do
+    IO.puts "*** Tests for len/1 fun ***"
+
+    [] |> ListModule.len |> IO.puts # 0
+    [1000] |> ListModule.len |> IO.puts # 1
+
+    [1, 2, 3, -123, "afsd", 555]
+      |> ListModule.len
+      |> IO.puts # 6
+
+    IO.puts "*** End of tests for len/1 func ***"
+  end
+
+  def test_range do
+    IO.puts "*** Tests for range/2 fun ***"
+
+    ListModule.range(1, 1) |> IO.inspect # []
+
+    ListModule.range('a', 2) |> IO.inspect # error hash
+    ListModule.range('a', []) |> IO.inspect # error hash
+    ListModule.range(10, {}) |> IO.inspect # error hash
+
+    ListModule.range(1, 3) |> IO.inspect # [1, 2, 3]
+    ListModule.range(10, 1) |> IO.inspect # [10, 9, 8...]
+    ListModule.range(1, 1000) |> IO.inspect # [1, 2, 3...]
+
+    IO.puts "*** End of tests for range/1 fun ***"
+  end
+end
+
+ListModuleTest.test
