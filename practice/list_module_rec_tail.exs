b48a5b455bdd145d92175555ecede979525bdd5c
fargelus <ddraudred@gmail.com>
2022-02-01 14:59:49 +0300

    Length of list with tail recursion

---
+++ practice/list_module_rec_tail.exs
@@ -0,0 +1,32 @@
+defmodule ListModule do
+  def rec_len(list) do
+    make_rec_len(list, 0)
+  end
+
+  defp make_rec_len([], size), do: size
+
+  defp make_rec_len([head | tail], size) do
+    make_rec_len(tail, size + 1)
+  end
+end
+
+defmodule ListModuleTest do
+  def test do
+    test_rec_len()
+  end
+
+  def test_rec_len do
+    IO.puts "*** Tests for rec_len/1 fun ***"
+
+    [] |> ListModule.rec_len |> IO.puts # 0
+    [1] |> ListModule.rec_len |> IO.puts # 1
+    [1, 2, 3, -123, "afsd", 555]
+      |> ListModule.rec_len
+      |> IO.puts # 6
+
+    Enum.to_list(1..100000000) |> ListModule.rec_len |> IO.puts
+    IO.puts "*** End of tests for len/1 func ***"
+  end
+end
+
+ListModuleTest.test
