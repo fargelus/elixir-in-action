45eb45adccb10e46bd3f90e87e7c720b3cf87f5d
fargelus <ddraudred@gmail.com>
2022-02-06 13:50:21 +0300

    Range with recursive tail optimisation

--- practice/list_module_rec_tail.exs
+++ practice/list_module_rec_tail.exs
@@ -3,6 +3,17 @@ defmodule ListModule do
     make_rec_len(list, 0)
   end
 
+  def rec_range(from, to) do
+    make_rec_range(from, to, [])
+  end
+
+  defp make_rec_range(to, to, list), do: [to | list]
+
+  defp make_rec_range(from, to, list) do
+    dec = if from < to, do: -1, else: 1
+    make_rec_range(from, to + dec, [to | list])
+  end
+
   defp make_rec_len([], size), do: size
 
   defp make_rec_len([_ | tail], size) do
@@ -13,10 +24,12 @@ end
 defmodule ListModuleTest do
   def test do
     test_rec_len()
+    IO.puts ""
+    test_req_range()
   end
 
   def test_rec_len do
-    IO.puts "*** Tests for rec_len/1 fun ***"
+    IO.puts "*** Tests for rec_len/0 fun ***"
 
     [] |> ListModule.rec_len |> IO.puts # 0
     [1] |> ListModule.rec_len |> IO.puts # 1
@@ -25,7 +38,19 @@ defmodule ListModuleTest do
       |> IO.puts # 6
 
     Enum.to_list(1..100000000) |> ListModule.rec_len |> IO.puts
-    IO.puts "*** End of tests for req_len/1 func ***"
+    IO.puts "*** End of tests for req_len/0 func ***"
+  end
+
+  def test_req_range do
+    IO.puts "*** Tests for rec_range/0 fun ***"
+
+    ListModule.rec_range(1, 1) |> IO.inspect # [1]
+    ListModule.rec_range(1, 10) |> IO.inspect # [1..10]
+    ListModule.rec_range(10, 1) |> IO.inspect # [10..1]
+    ListModule.rec_range(1, 100) |> IO.inspect(limit: :infinity)
+    ListModule.rec_range(1, 1000000) |> IO.inspect(limit: :infinity)
+
+    IO.puts "*** End of tests for rec_range/0 fun ***"
   end
 end
 
