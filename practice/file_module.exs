9f8205ff1293a96667fbff60c11260a323f72bd8
fargelus <ddraudred@gmail.com>
2022-02-20 13:10:23 +0300

    Some review fixes

--- practice/file_module.exs
+++ practice/file_module.exs
@@ -1,6 +1,6 @@
 defmodule FileModule do
   def line_lengths!(path) do
-    lines!(path) |> Enum.map(fn line -> String.length(line) end)
+    lines!(path) |> Enum.map(&String.length/1)
   end
 
   def longest_line_length!(path) do
@@ -8,10 +8,10 @@ defmodule FileModule do
   end
 
   def longest_line!(path) do
-    lines!(path) |> Enum.max_by(fn line -> String.length(line) end)
+    lines!(path) |> Enum.max_by(&String.length/1)
   end
 
-  def words_per_line(path) do
+  def words_per_line!(path) do
     lines!(path) |> Enum.map(&count_words_per_line/1)
   end
 
@@ -21,7 +21,7 @@ defmodule FileModule do
     line_lengths!(file_path) |> IO.inspect(limit: :infinity)
     longest_line_length!(file_path) |> IO.puts
     longest_line!(file_path) |> IO.puts
-    words_per_line(file_path) |> IO.inspect
+    words_per_line!(file_path) |> IO.inspect
   end
 
   defp lines!(path) do
